afd9765176c869ba3f477122cdabe929748db37a
Andrei Ponomarenko <3355844@gmail.com>
2017-12-18 16:40:37 +0200

    Add User

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// GET users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
@@ -15,4 +15,17 @@ router.get('/userlist', function (req, res) {
         res.json(docs);
     })
 });
+
+ // POST to adduser
+
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
 module.exports = router;
