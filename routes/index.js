783255b6d7340661392148c2fffa3352cf5e9792
Andrei Ponomarenko <3355844@gmail.com>
2017-12-18 18:12:18 +0200

    Mongodb connect from mlab

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WACh', user: 'Andrii' });
+  res.render('index', { title: 'WACh' });
 });
 
 module.exports = router;
