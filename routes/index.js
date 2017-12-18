1ee3cbb5464117ac20f7716197ba6e2e310be9b6
Andrei Ponomarenko <3355844@gmail.com>
2017-12-18 15:59:03 +0200

    Add userList

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WACh', userName: 'Andrii' });
+  res.render('index', { title: 'WACh', user: 'Andrii' });
 });
 
 module.exports = router;
